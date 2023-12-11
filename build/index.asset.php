212dc0342474b3d8328b4fc66cb8f8cfd458f3e7
wictor kanerva <kanervawictor@gmail.com>
2023-12-11 22:34:58 +0100

    test

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '163ad5e35b524e3f1606');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0a32fc6becf6a6736e28');
